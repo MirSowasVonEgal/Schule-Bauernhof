84d929677307c840be3bd0ae807fbbf07e77fc63
Timo <timootten@icloud.com>
2022-09-15 14:00:48 +0200

    Updated

--- src/me/timo/bauernhof/Main.java
+++ src/me/timo/bauernhof/Main.java
@@ -9,6 +9,13 @@ public class Main {
         tierliste.add(new Tier( "Schaf", false, ""));
         Hof hof = new Hof("Wiesenstraße 1", 50);
         Bauer bauer = new Bauer("Hans", tierliste, hof);
+        System.out.println("Der Hof hat " + bauer.getHofGröße() + " Quadratmeter");
+        System.out.println("Der Hof wird vergrößert");
+        bauer.feldKaufen();
+        System.out.println("Der Hof hat " + bauer.getHofGröße() + " Quadratmeter");
+        for (Tier tier : bauer.getTierliste()) {
+            tier.begrüßen();
+        }
         System.out.println("Tier Liste:");
         for (Tier tier : bauer.getTierliste()) {
             System.out.println("- " + tier.getName() + " ist " + (tier.isSatt() ? "satt" : "nicht satt"));
@@ -19,13 +26,6 @@ public class Main {
         for (Tier tier : bauer.getTierliste()) {
             System.out.println("- " + tier.getName() + " ist " + (tier.isSatt() ? "satt" : "nicht satt"));
         }
-        for (Tier tier : bauer.getTierliste()) {
-            tier.begrüßen();
-        }
-        System.out.println("Der Hof hat " + bauer.getHofGröße() + " Quadratmeter");
-        System.out.println("Der Hof wird vergrößert");
-        bauer.feldKaufen();
-        System.out.println("Der Hof hat " + bauer.getHofGröße() + " Quadratmeter");
     }
 
 }
